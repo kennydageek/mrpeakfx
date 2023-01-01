cdb4855e7eac25486edbbf8f031726f3b3550aec
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2023-01-01 13:44:31 +0100

    configurations, and setting up of vuetify library

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import upperFirst from 'lodash/upperFirst';
+import camelCase from 'lodash/camelCase';
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
+// Global registration of components
+const requireComponent = require.context(
+  './components/global',
+  true,
+  /\.(js|vue)$/i
+);
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  );
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
+
 new Vue({
+  vuetify,
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
